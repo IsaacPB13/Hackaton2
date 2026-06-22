b24a77c4a4df967abf923baba0b8d70eb6b55e04
IsaacPB13 <iperezb1308@gmail.com>
2026-06-22 10:55:57 -0600

    Agregando archivos base

---
+++ src/AgendaTelefonica.java
@@ -0,0 +1,39 @@
+public class AgendaTelefonica {
+
+    Contacto list[] = new Contacto[10];
+
+    public void existeContacto(Contacto c){
+
+
+    }
+
+    public void listarContactos(){
+
+
+    }
+
+    public void buscaContacto(String nombre, String apellido){
+
+
+    }
+
+    public void eliminarContacto(Contacto c){
+
+
+    }
+
+    public void modificarTelefono(String nombre, String apellido, String nuevoTelefono){
+
+
+    }
+
+    public void agendaLlena(){
+
+
+    }
+
+    public void espacioLibres(){
+
+
+    }
+}
